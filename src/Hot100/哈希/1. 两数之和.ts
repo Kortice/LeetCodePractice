240b61e0487af60d2fa8d78799ff955cf9864a41
Kortice <kortic@foxmail.com>
2025-11-01 00:00:30 +0800

    first commit

---
+++ src/Hot100/哈希/1. 两数之和.ts
@@ -0,0 +1,18 @@
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ */
+
+function twoSum(nums: number[], target: number): number[] {
+  let res: number[] = []
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        res.push(i, j)
+        break;
+      }
+    }
+  }
+
+  return res
+};
